cc148ab3a14b36c6eead702de6a16fd55ab15272
Sergio Herrera Guzmán <sergio@sergiohgz.eu>
2020-07-05 18:37:39 +0200

    feat(ex): create a middleware cache using decorators

--- server/decorators.go
+++ server/decorators.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/http/httputil"
 	"time"
+
+	"github.com/pabloos/http/greet"
 )
 
 func POST(h http.HandlerFunc) http.HandlerFunc {
@@ -36,5 +41,30 @@ func Delay(delay time.Duration, h http.HandlerFunc) http.HandlerFunc {
 		defer h.ServeHTTP(w, r)
 
 		time.Sleep(delay)
+	}
+}
+
+func Cache(h http.HandlerFunc) http.HandlerFunc {
+	messages := make(map[string]greet.Greet)
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Read body into buffer to avoid consuming it - https://stackoverflow.com/questions/23070876/reading-body-of-http-request-without-modifying-request-state
+		bufferBody, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			return
+		}
+
+		r.Body = ioutil.NopCloser(bytes.NewBuffer(bufferBody))
+		defer h.ServeHTTP(w, r)
+
+		var parsedBody greet.Greet
+		body := ioutil.NopCloser(bytes.NewBuffer(bufferBody))
+		json.NewDecoder(body).Decode(&parsedBody)
+		cacheData, found := messages[parsedBody.Name]
+		if found {
+			fmt.Fprintf(w, "%s, from %s was found in cache\n", cacheData.Name, cacheData.Location)
+		} else {
+			messages[parsedBody.Name] = parsedBody
+		}
 	}
 }
--- server/mux.go
+++ server/mux.go
@@ -2,15 +2,14 @@ package main
 
 import (
 	"net/http"
-	"time"
 )
 
 func newMux() http.Handler {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", Debug(index))
-	mux.HandleFunc("/greet", Delay(2*time.Second, POST(greetHandler)))
-	// mux.HandleFunc("/greet", POST(greetHandler))
+	// mux.HandleFunc("/greet", Delay(2*time.Second, POST(greetHandler)))
+	mux.HandleFunc("/greet", Cache(POST(greetHandler)))
 
 	return mux
 }
